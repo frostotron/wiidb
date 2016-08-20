59a3047706e56304ceaac1e70f9ec539adf73f4a
Frosto Tron <frostotron@post.com>
2016-08-20 14:35:47 -0500

    Packaged with setuptools.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+    name = 'wiidb',
+    version = '0.1',
+    author = 'frostotron',
+    author_email = 'frostotron@post.com',
+    description = 'A Python library for downloading and processing wiitdb.zip from gametdb.com',
+    license = 'GPL3',
+    packages = ['wiidb']
+)
